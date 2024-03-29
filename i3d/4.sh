5f3507c3bccf51f83baab119a7ee83ae6b014f2e
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-03-29 09:13:19 -0600

    Update 4.sh

--- i3d/4.sh
+++ i3d/4.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Install Ruby
-sudo apt install -y ruby-full
+apt install -y ruby-full
 
 # Add rbenv to PATH and initialize rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 source ~/.bashrc
 
-sudo apt-get update
+# Update package repositories
+apt-get update
+
 # Inform user about installation completion
 echo "################################################################"
 echo "###################    Installation Done  ######################"
