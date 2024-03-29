08e120f60583b08d4db9018caac0643b07168228
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-03-29 05:36:02 -0600

    Update 4.sh

--- i3d/4.sh
+++ i3d/4.sh
@@ -8,20 +8,7 @@ echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 source ~/.bashrc
 
-# Change permissions and execute 5.sh if it exists
-if [ -f "5.sh" ]; then
-    # Check if the script is not executable
-    if ! [ -x "5.sh" ]; then
-        # Change permissions to make the script executable
-        chmod +x 5.sh
-    fi
-    
-    # Execute the script
-    sudo ./5.sh
-else
-    echo "Script '5.sh' not found. Skipping execution."
-fi
-
+sudo apt-get update
 # Inform user about installation completion
 echo "################################################################"
 echo "###################    Installation Done  ######################"
